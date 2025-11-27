b54e53d3dd5d930458429cd91c9b3edacb445469
Matheus <matheussenac2026@gmail.com>
2025-11-27 19:57:44 -0300

    Foi renomeado os blocos da Frm

--- FrmCadastrodeProduto.Designer.cs
+++ FrmCadastrodeProduto.Designer.cs
@@ -28,139 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label2 = new Label();
-            comboBox1 = new ComboBox();
-            label3 = new Label();
-            comboBox2 = new ComboBox();
-            label4 = new Label();
-            comboBox3 = new ComboBox();
-            label5 = new Label();
-            comboBox4 = new ComboBox();
-            button1 = new Button();
-            groupBox1 = new GroupBox();
-            groupBox1.SuspendLayout();
+            lblNomeProduto = new Label();
+            txtNomeProduto = new ComboBox();
+            lblCodigoProduto = new Label();
+            txtCodigoProduto = new ComboBox();
+            lblDataProduto = new Label();
+            txtDataProduto = new ComboBox();
+            lblPrecoProduto = new Label();
+            txtPrecoProduto = new ComboBox();
+            bntSalvar = new Button();
+            grbCadastrarProdutos = new GroupBox();
+            grbCadastrarProdutos.SuspendLayout();
             SuspendLayout();
             // 
-            // label2
+            // lblNomeProduto
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(6, 32);
-            label2.Name = "label2";
-            label2.Size = new Size(103, 15);
-            label2.TabIndex = 1;
-            label2.Text = "Nome do Produto";
+            lblNomeProduto.AutoSize = true;
+            lblNomeProduto.Location = new Point(6, 32);
+            lblNomeProduto.Name = "lblNomeProduto";
+            lblNomeProduto.Size = new Size(103, 15);
+            lblNomeProduto.TabIndex = 1;
+            lblNomeProduto.Text = "Nome do Produto";
             // 
-            // comboBox1
+            // txtNomeProduto
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(6, 50);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(684, 23);
-            comboBox1.TabIndex = 2;
+            txtNomeProduto.FormattingEnabled = true;
+            txtNomeProduto.Location = new Point(6, 50);
+            txtNomeProduto.Name = "txtNomeProduto";
+            txtNomeProduto.Size = new Size(684, 23);
+            txtNomeProduto.TabIndex = 2;
             // 
-            // label3
+            // lblCodigoProduto
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(6, 107);
-            label3.Name = "label3";
-            label3.Size = new Size(109, 15);
-            label3.TabIndex = 3;
-            label3.Text = "Código do Produto";
+            lblCodigoProduto.AutoSize = true;
+            lblCodigoProduto.Location = new Point(6, 107);
+            lblCodigoProduto.Name = "lblCodigoProduto";
+            lblCodigoProduto.Size = new Size(109, 15);
+            lblCodigoProduto.TabIndex = 3;
+            lblCodigoProduto.Text = "Código do Produto";
             // 
-            // comboBox2
+            // txtCodigoProduto
             // 
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(6, 125);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(140, 23);
-            comboBox2.TabIndex = 4;
+            txtCodigoProduto.FormattingEnabled = true;
+            txtCodigoProduto.Location = new Point(6, 125);
+            txtCodigoProduto.Name = "txtCodigoProduto";
+            txtCodigoProduto.Size = new Size(140, 23);
+            txtCodigoProduto.TabIndex = 4;
             // 
-            // label4
+            // lblDataProduto
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(180, 107);
-            label4.Name = "label4";
-            label4.Size = new Size(79, 15);
-            label4.TabIndex = 5;
-            label4.Text = "Data_Produto";
+            lblDataProduto.AutoSize = true;
+            lblDataProduto.Location = new Point(172, 107);
+            lblDataProduto.Name = "lblDataProduto";
+            lblDataProduto.Size = new Size(94, 15);
+            lblDataProduto.TabIndex = 5;
+            lblDataProduto.Text = "Data do Produto";
             // 
-            // comboBox3
+            // txtDataProduto
             // 
-            comboBox3.FormattingEnabled = true;
-            comboBox3.Location = new Point(180, 125);
-            comboBox3.Name = "comboBox3";
-            comboBox3.Size = new Size(148, 23);
-            comboBox3.TabIndex = 6;
+            txtDataProduto.FormattingEnabled = true;
+            txtDataProduto.Location = new Point(172, 125);
+            txtDataProduto.Name = "txtDataProduto";
+            txtDataProduto.Size = new Size(148, 23);
+            txtDataProduto.TabIndex = 6;
             // 
-            // label5
+            // lblPrecoProduto
             // 
-            label5.AutoSize = true;
-            label5.Location = new Point(345, 107);
-            label5.Name = "label5";
-            label5.Size = new Size(100, 15);
-            label5.TabIndex = 7;
-            label5.Text = "Preço do Produto";
-            label5.Click += label5_Click;
+            lblPrecoProduto.AutoSize = true;
+            lblPrecoProduto.Location = new Point(343, 107);
+            lblPrecoProduto.Name = "lblPrecoProduto";
+            lblPrecoProduto.Size = new Size(100, 15);
+            lblPrecoProduto.TabIndex = 7;
+            lblPrecoProduto.Text = "Preço do Produto";
+            lblPrecoProduto.Click += label5_Click;
             // 
-            // comboBox4
+            // txtPrecoProduto
             // 
-            comboBox4.FormattingEnabled = true;
-            comboBox4.Location = new Point(345, 125);
-            comboBox4.Name = "comboBox4";
-            comboBox4.Size = new Size(148, 23);
-            comboBox4.TabIndex = 8;
+            txtPrecoProduto.FormattingEnabled = true;
+            txtPrecoProduto.Location = new Point(343, 125);
+            txtPrecoProduto.Name = "txtPrecoProduto";
+            txtPrecoProduto.Size = new Size(148, 23);
+            txtPrecoProduto.TabIndex = 8;
             // 
-            // button1
+            // bntSalvar
             // 
-            button1.Location = new Point(698, 405);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 9;
-            button1.Text = "Salvar";
-            button1.UseVisualStyleBackColor = true;
+            bntSalvar.Location = new Point(713, 356);
+            bntSalvar.Name = "bntSalvar";
+            bntSalvar.Size = new Size(75, 23);
+            bntSalvar.TabIndex = 9;
+            bntSalvar.Text = "Salvar";
+            bntSalvar.UseVisualStyleBackColor = true;
             // 
-            // groupBox1
+            // grbCadastrarProdutos
             // 
-            groupBox1.Controls.Add(comboBox1);
-            groupBox1.Controls.Add(label2);
-            groupBox1.Controls.Add(comboBox4);
-            groupBox1.Controls.Add(label3);
-            groupBox1.Controls.Add(label5);
-            groupBox1.Controls.Add(comboBox2);
-            groupBox1.Controls.Add(comboBox3);
-            groupBox1.Controls.Add(label4);
-            groupBox1.Location = new Point(12, 12);
-            groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(776, 318);
-            groupBox1.TabIndex = 10;
-            groupBox1.TabStop = false;
-            groupBox1.Text = "Cadastrar Produtos";
+            grbCadastrarProdutos.Controls.Add(txtNomeProduto);
+            grbCadastrarProdutos.Controls.Add(lblNomeProduto);
+            grbCadastrarProdutos.Controls.Add(txtPrecoProduto);
+            grbCadastrarProdutos.Controls.Add(lblCodigoProduto);
+            grbCadastrarProdutos.Controls.Add(lblPrecoProduto);
+            grbCadastrarProdutos.Controls.Add(txtCodigoProduto);
+            grbCadastrarProdutos.Controls.Add(txtDataProduto);
+            grbCadastrarProdutos.Controls.Add(lblDataProduto);
+            grbCadastrarProdutos.Location = new Point(12, 12);
+            grbCadastrarProdutos.Name = "grbCadastrarProdutos";
+            grbCadastrarProdutos.Size = new Size(776, 318);
+            grbCadastrarProdutos.TabIndex = 10;
+            grbCadastrarProdutos.TabStop = false;
+            grbCadastrarProdutos.Text = "Cadastrar Produtos";
             // 
             // FrmCadastrodeProduto
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.CornflowerBlue;
-            ClientSize = new Size(800, 450);
-            Controls.Add(groupBox1);
-            Controls.Add(button1);
+            ClientSize = new Size(799, 397);
+            Controls.Add(grbCadastrarProdutos);
+            Controls.Add(bntSalvar);
             Name = "FrmCadastrodeProduto";
             Text = "Cadastro De Produtos";
-            groupBox1.ResumeLayout(false);
-            groupBox1.PerformLayout();
+            grbCadastrarProdutos.ResumeLayout(false);
+            grbCadastrarProdutos.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
-        private Label label2;
-        private ComboBox comboBox1;
-        private Label label3;
-        private ComboBox comboBox2;
-        private Label label4;
-        private ComboBox comboBox3;
-        private Label label5;
-        private ComboBox comboBox4;
-        private Button button1;
-        private GroupBox groupBox1;
+        private Label lblNomeProduto;
+        private ComboBox txtNomeProduto;
+        private Label lblCodigoProduto;
+        private ComboBox txtCodigoProduto;
+        private Label lblDataProduto;
+        private ComboBox txtDataProduto;
+        private Label lblPrecoProduto;
+        private ComboBox txtPrecoProduto;
+        private Button bntSalvar;
+        private GroupBox grbCadastrarProdutos;
     }
 }
